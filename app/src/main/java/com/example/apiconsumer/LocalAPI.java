8258594132c4b16a98e7ba7e03bec2fecc942915
Álvaro Zamora <alvarozajim@gmail.com>
2020-01-23 19:01:38 +0100

    get and delete local api

--- app/src/main/java/com/example/apiconsumer/LocalAPI.java
+++ app/src/main/java/com/example/apiconsumer/LocalAPI.java
@@ -3,6 +3,10 @@ package com.example.apiconsumer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +25,13 @@ public class LocalAPI extends AppCompatActivity {
     apiService service;
     Gson gson = new Gson();
 
+    Button del;
+    Button post;
+
+    EditText id;
+    EditText name;
+    EditText surname;
+
     TextView text1;
 
     @Override
@@ -29,15 +40,20 @@ public class LocalAPI extends AppCompatActivity {
         setContentView(R.layout.activity_local_api);
 
         text1 = findViewById(R.id.text);
+        id = findViewById(R.id.idC);
+        name = findViewById(R.id.cName);
+        surname = findViewById(R.id.cSurname);
+        del = findViewById(R.id.delC);
+        post = findViewById(R.id.postC);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://localhost:44311/")
+                .baseUrl("http://10.0.2.2:44311/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         service = retrofit.create(apiService.class);
 
-        Call<Contact> callAsync = service.getContact(0);
+        Call<Contact> callAsync = service.getContact(3);
 
         callAsync.enqueue(new Callback<Contact>()
         {
@@ -45,6 +61,7 @@ public class LocalAPI extends AppCompatActivity {
             @Override
             public void onResponse(Call<Contact> call, Response<Contact> response)
             {
+                Log.e("xd", call.request().url().toString());
                 if (response.body() != null)
                 {
 
@@ -68,5 +85,61 @@ public class LocalAPI extends AppCompatActivity {
                 System.out.println("Network Error :: " + t.getLocalizedMessage());
             }
         });
+    }
+
+    public void doPost(View view) {
+
+        Call<Contact> callAsync = service.postContact();
+
+        callAsync.enqueue(new Callback<Contact>()
+        {
+
+            @Override
+            public void onResponse(Call<Contact> call, Response<Contact> response)
+            {
+                Log.e("xd", call.request().url().toString());
+                Log.e("xd", "good");
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Contact deleted.",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Contact> call, Throwable t)
+            {
+                System.out.println("Network Error :: " + t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    public void doDel(View view) {
+
+        Log.e("xd", Integer.parseInt(id.getText().toString())+"");
+
+        Call<Contact> callAsync = service.delContact(Integer.parseInt(id.getText().toString()));
+
+        callAsync.enqueue(new Callback<Contact>()
+        {
+
+            @Override
+            public void onResponse(Call<Contact> call, Response<Contact> response)
+            {
+                Log.e("xd", call.request().url().toString());
+                Log.e("xd", "good");
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Contact deleted.",
+                            Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Contact> call, Throwable t)
+            {
+                System.out.println("Network Error :: " + t.getLocalizedMessage());
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/apiconsumer/apiService.java
+++ app/src/main/java/com/example/apiconsumer/apiService.java
@@ -5,7 +5,10 @@ import com.google.gson.JsonElement;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -19,4 +22,12 @@ public interface apiService {
 
     @GET("contacte/{id}")
     Call<Contact> getContact(@Path("id") int id);
+
+    @POST("contacte")
+    Call<Contact> postContact(@Body Contact c);
+
+    @DELETE("contacteTot/{id}")
+    Call<Contact> delContact(@Path("id") int id);
+
+
 }
