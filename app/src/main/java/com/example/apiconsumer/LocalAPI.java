6a7e6b2fe0cbef18c2a4feac1c47b5cb5a5b5e05
Álvaro Zamora <alvarozajim@gmail.com>
2020-01-23 16:26:41 +0100

    second activity for local api

---
+++ app/src/main/java/com/example/apiconsumer/LocalAPI.java
@@ -0,0 +1,14 @@
+package com.example.apiconsumer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalAPI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_api);
+    }
+}
